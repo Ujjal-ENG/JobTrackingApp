d9dae91bb46d7364c934760536b7f927448d03d1
user <ujjalroy7862@gmail.com>
2022-12-03 17:20:28 +0600

    handle the all type of backed errors

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,8 +1,21 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("Please Provide all Values");
+  }
+
+  const userAlreadyExists = await User.findOne({ email });
+
+  if (userAlreadyExists) {
+    throw new BadRequestError('Email Already an Used')
+  }
+
+  const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
--- middleware/error.handler.js
+++ middleware/error.handler.js
@@ -1,12 +1,23 @@
 import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleWare = (err, req, res, next) => {
-  console.log(err);
+  console.log(err.message);
   const defaultError = {
-    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: "Something went wrong, try again later",
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong, try again later",
   };
-  res.status(defaultError.statusCode).json({ msg: err });
+  if (err.name === "ValidatorError") {
+    defaultError.statusCode === StatusCodes.BAD_REQUEST;
+    // defaultError.msg = err.message;
+    defaultError.nsg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+  }
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
+  }
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleWare;
